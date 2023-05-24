e1964d2dd606cc882d6faa6909a822286345d4ab
Syful Sharif <sharif.syful@gmail.com>
2023-05-25 00:12:48 +0600

    Class and Function Components Added along with their CSS

---
+++ src/AppClass.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import './AppClass.css';
+
+export default class AppClass extends Component {
+    render() {
+        return(
+            <h1 className="h1-red"> Hello, World!</h1>
+        );
+    }
+}
---
+++ src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import './HelloWorld.css';
+
+function HelloWorld() {
+    return (
+        <Fragment>
+            <hr></hr>
+            <h1 className="h1-green">Hello, World!</h1>
+        </Fragment>
+    );
+}
+
+export default HelloWorld;
