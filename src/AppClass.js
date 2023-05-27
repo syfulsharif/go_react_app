fa2290848eb6e3016afb18c248eacf43c0ee00ed
Syful Sharif <sharif.syful@gmail.com>
2023-05-27 20:25:00 +0600

    State 2 added

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,10 +1,50 @@
 import React, { Component } from "react";
-import './AppClass.css';
+import "./AppClass.css";
 
 export default class AppClass extends Component {
-    render() {
-        return(
-            <h1 className="h1-red">{this.props.msg}</h1>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTrue: false,
+    };
+  }
+
+  toggleTrue = () => {
+    if (this.state.isTrue) {
+      this.setState({
+        isTrue: false,
+      });
+      return;
     }
+
+    this.setState({
+      isTrue: true,
+    });
+  };
+  render() {
+    return (
+      <>
+        <hr></hr>
+        <h1 className="h1-green">{this.props.msg}</h1>
+        <hr></hr>
+        {this.state.isTrue && (
+          <>
+            <p>The current value of isTrue is True</p>
+            <hr></hr>
+          </>
+        )}
+        <hr></hr>
+        <hr></hr>
+        {this.state.isTrue ? <p>isTrue is True</p> : <p>isTrue is False</p>}
+        <a
+          href="#!"
+          className="btn btn-outline-secondary"
+          onClick={this.toggleTrue}
+        >
+          {" "}
+          Toggle isTrue
+        </a>
+      </>
+    );
+  }
 }
