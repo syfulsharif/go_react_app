108d384a5cc0780be5021eb148f18608fe2c3d09
Syful Sharif <sharif.syful@gmail.com>
2023-05-27 19:33:34 +0600

    State management I is introduced

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,13 +1,35 @@
-import React, { Fragment } from "react";
-import './HelloWorld.css';
+import React, { Fragment, useState } from "react";
+import "./HelloWorld.css";
 
-function HelloWorld() {
-    return (
+function HelloWorld(props) {
+  const [isTrue, setIsTrue] = useState(true);
+  const toggleTrue = () => {
+    if (isTrue) {
+        setIsTrue(false);
+        return;
+    }
+     setIsTrue(true);
+  }
+  return (
+    <Fragment>
+      <hr></hr>
+      <h1 className="h1-green">{props.msg}</h1>
+      <hr></hr>
+      {isTrue && (
         <Fragment>
-            <hr></hr>
-            <h1 className="h1-green">Hello, World!</h1>
+          <p>The current value of isTrue is True</p>
+          <hr></hr>
         </Fragment>
-    );
+      )}
+      <hr></hr>
+      <hr></hr>
+      {isTrue
+         ? <p>isTrue is True</p>
+         : <p>isTrue is False</p>
+      }
+      <a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}> Toggle isTrue</a>
+    </Fragment>
+  );
 }
 
 export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppClass from './AppClass';
 import HelloWorld from './HelloWorld';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppClass />
-    <HelloWorld />
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <HelloWorld msg="Hello, again!" />
+        </div>
+      </div>
+    </div>
+
   </React.StrictMode>
 );
 
